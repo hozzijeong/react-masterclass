e386f89ef11facecc8a31afe12ce5e9b4c84dc59
hozzijeong <ghwls0905@naver.com>
2022-01-12 17:29:35 +0900

    Feat: TypeScript State적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
-import { Wrapper, Box } from "./AppStyled";
+import React, { useState } from "react";
+import styled from "styled-components";
+
+
+const Wrapperr = styled.div`
+    display: flex;
+    width: 100vw;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    background-color: ${props => props.theme.bgColor};
+`
+
+const Input = styled.input`
+    color: ${props => props.theme.textColor};
+    background-color: ${props=>props.theme.bgColor};
+    text-align:center;
+    outline:none;
+    border:none;
+`
 
 function App() {
+    const [value,setValue] = useState<string|number>("");
+    const onChange = (event:React.FormEvent<HTMLInputElement>) => {
+        setValue(event.currentTarget.value);
+        console.log(event.target,event.currentTarget)
+    }
+
     return (
-        <Wrapper>
-            <Box>
-            </Box>
-        </Wrapper>
+        <Wrapperr>
+            <Input value={value} type='text' onChange={onChange}/>
+        </Wrapperr>
     );
 }
 
